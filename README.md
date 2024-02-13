aedc1ff974be6c066e2fa59edf809eedbe7b7112
IgorTraspadini <igorchieppe@hotmail.com>
2024-02-13 08:02:14 -0300

    README file and html sample file

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Team-Profile-Generator-ICT
-Create an application that generates HTML files using input from a command-line interface.
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# Team Profile Generator
+
+## Description
+This application will take in information about employees on a software engineering team, then generate an HTML webpage using Node.js command-line application, that displays summaries for each person. It have been provided with tests, so make sure every part of the code passes each provided test.
+
+## Language
+- ES6
+- POO JavaScript
+- HTML
+- Bootstrap
+- Node.js
+- inquirer package
+
+## Rum Project
+```bash
+# Clone the reposotiry 
+git clone https://github.com/IgorTraspadini/Team-Profile-Generator-ICT.git
+
+# install the node's modules
+npm install
+
+# Run the project
+node index.js
+```
+## Tests
+
+
+## Links
+- [HTML sample file](https://github.com/IgorTraspadini/Team-Profile-Generator-ICT/blob/main/output/team.html)
+
+## References 
+- [Node.js](https://nodejs.org/en)
+- [npm inquirer](https://www.npmjs.com/package/inquirer)
+- [ES6](https://www.w3schools.com/Js/js_es6.asp)
+- [POO](https://www.w3schools.com/Js/js_classes.asp)
+
+## Credits
+[edX - Bootcamp in Front-End Web Development](https://www.edx.org/course/skills-bootcamp-in-front-end-web-development?parent_component=new-on-edx&webview=false&campaign=Skills+Bootcamp+in+Front-End+Web+Development&source=edx&product_category=boot-camp&placement_url=https%3A%2F%2Fwww.edx.org%2F)
+
--- src/page-template.js
+++ src/page-template.js
@@ -4,7 +4,7 @@ const generateTeam = team => {
     // creates the manager html
     const generateManager = manager => {
         return `
-        <div class="card employee-card" style="border-radius: 1rem">
+        <div class="card employee-card m-2" style="box-shadow: 5px 5px 5px lightblue;">
         <div class="card-header" style="background-color: lightblue;">
             <h2 class="card-title">${manager.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
@@ -23,7 +23,7 @@ const generateTeam = team => {
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card" style="border-radius: 1rem">
+        <div class="card employee-card m-2" style="box-shadow: 5px 5px 5px cadetblue;">
     <div class="card-header" style="background-color: cadetblue;">
         <h2 class="card-title">${engineer.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
@@ -42,8 +42,8 @@ const generateTeam = team => {
     // creates the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card" style="border-radius: 1rem">
-    <div class="card-header">
+        <div class="card employee-card m-2" style="box-shadow: 5px 5px 5px aquamarine;">
+    <div class="card-header" style="background-color: aquamarine;">
         <h2 class="card-title">${intern.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
     </div>
